ab37bd69dade4558f375d0c938d54b510667650a
ousnius <denis41@hotmail.de>
2015-05-03 22:37:33 +0200

    Resource updated

--- Resource.h
+++ Resource.h
@@ -1,5 +1,17 @@
-﻿// Microsoft Visual C++ generated include file.
-// Used by BodySlide.rc
-
+//{{NO_DEPENDENCIES}}
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch BodySlide.rc
+//
 #define IDI_ICON1                       101
 #define AAAAIcon                        101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
