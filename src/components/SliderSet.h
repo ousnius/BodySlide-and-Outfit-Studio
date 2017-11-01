ousnius/BodySlide-and-Outfit-Studio

commit 35e56c34b1482c3e8216dc454c654bd0d1c49779

Seam normals per shape #160 and sliderset cleanup

src/components/SliderSet.h
@@ -12,8 +12,13 @@ See the included LICENSE file
 
 using namespace tinyxml2;
 
-class SliderSet
-{
+struct SliderSetShape {
+	std::string targetShape;								// Target names mapped to nif file shape names.
+	std::string dataFolder;
+	bool smoothSeamNormals = true;
+};
+
+class SliderSet {
 	std::string name;
 	std::string baseDataPath;								// Base data path - from application configuration.
 	std::string datafolder;									// Default data folder specified for a slider set (overridden by target data folders, usually).
@@ -21,8 +26,8 @@ class SliderSet
 	std::string outputpath;
 	std::string outputfile;
 	bool genWeights = false;								// Generate both low and high weight meshes on output.
-	std::map<std::string, std::string> targetshapenames;	// Target names mapped to nif file shape names.
-	std::map<std::string, std::string> targetdatafolders;
+
+	std::map<std::string, SliderSetShape> shapeAttributes;
 
 	std::vector<SliderData> sliders;
 	std::vector<NormalGenLayer> defNormalGen;
@@ -53,16 +58,10 @@ public:
 		genWeights = inGenWeights;
 	}
 
-	int GetTargetShapeCount() {
-		return targetshapenames.size();
-	}
-
 	void Clear() {
-		targetshapenames.clear();
-		targetdatafolders.clear();
+		shapeAttributes.clear();
 		sliders.clear();
 	}
-
 
 	void SetBaseDataPath(const std::string& inPath) {
 		baseDataPath = inPath;
@@ -99,14 +98,35 @@ public:
 	std::string GetDefaultDataFolder() {
 		return datafolder;
 	}
+
 	bool GenWeights();
+
+	bool GetSmoothSeamNormals(const std::string& shapeName) {
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end())
+			return shape->second.smoothSeamNormals;
+
+		return true;
+	}
+
+	void SetSmoothSeamNormals(const std::string& shapeName, const bool smooth) {
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end())
+			shape->second.smoothSeamNormals = smooth;
+	}
+
+	void ToggleSmoothSeamNormals(const std::string& shapeName) {
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end())
+			shape->second.smoothSeamNormals = !shape->second.smoothSeamNormals;
+	}
 
 	// Gets the target names in the targetdatafolders map - these are the shapes with non-local or referenced data.
 	// Use TargetToShape to get the NIF file shape name.
 	void GetReferencedTargets(std::vector<std::string> &outTargets) {
-		for (auto &tdf : targetdatafolders)
-			if (tdf.second != datafolder)
-				outTargets.push_back(tdf.first);
+		for (auto &s : shapeAttributes)
+			if (s.second.dataFolder != datafolder)
+				outTargets.push_back(s.second.targetShape);
 	}
 
 	void SetReferencedData(const std::string& shapeName, const bool local = false) {
@@ -137,15 +157,20 @@ public:
 	}
 
 	std::string TargetToShape(const std::string& targetName) {
-		if (targetshapenames.find(targetName) != targetshapenames.end())
-			return targetshapenames[targetName];
+		for (auto &s : shapeAttributes)
+			if (s.second.targetShape == targetName)
+				return s.first;
 
 		return "";
 	}
 
 	void ClearTargets(const std::string& oldTarget) {
-		targetshapenames.erase(oldTarget);
-		targetdatafolders.erase(oldTarget);
+		for (auto &s : shapeAttributes) {
+			if (s.second.targetShape == oldTarget) {
+				s.second.targetShape.clear();
+				s.second.dataFolder.clear();
+			}
+		}
 	}
 
 	void Retarget(const std::string& oldTarget, const std::string& newTarget) {
@@ -156,60 +181,60 @@ public:
 	}
 
 	void AddShapeTarget(const std::string& shapeName, const std::string& targetName) {
-		targetshapenames[targetName] = shapeName;
+		auto& shape = shapeAttributes[shapeName];
+		shape.targetShape = targetName;
 	}
 
 	void RenameShape(const std::string& shapeName, const std::string& newShapeName) {
-		std::string target = ShapeToTarget(shapeName);
-		if (!target.empty()) {
-			if (targetshapenames.find(target) != targetshapenames.end()) {
-				targetshapenames[newShapeName] = newShapeName;
-				targetshapenames.erase(target);
-			}
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end()) {
+			for (auto& slider : sliders)
+				slider.RenameTarget(shape->second.targetShape, newShapeName);
 
-			if (targetdatafolders.find(target) != targetdatafolders.end()) {
-				targetdatafolders[newShapeName] = targetdatafolders[target];
-				targetdatafolders.erase(target);
-			}
+			shapeAttributes[newShapeName] = shape->second;
+			shapeAttributes.erase(shapeName);
 		}
-
-		for (auto& slider : sliders)
-			slider.RenameTarget(target, newShapeName);
 	}
 
 	void AddTargetDataFolder(const std::string& targetName, const std::string& dataFolder) {
-		targetdatafolders[targetName] = dataFolder;
+		for (auto &s : shapeAttributes)
+			if (s.second.targetShape == targetName)
+				s.second.dataFolder = dataFolder;
 	}
 
-	std::map<std::string, std::string>::iterator TargetShapesBegin() {
-		return targetshapenames.begin();
+	std::map<std::string, SliderSetShape>::const_iterator ShapesBegin() {
+		return shapeAttributes.cbegin();
 	}
-	std::map<std::string, std::string>::iterator TargetShapesEnd() {
-		return targetshapenames.end();
+	std::map<std::string, SliderSetShape>::const_iterator ShapesEnd() {
+		return shapeAttributes.cend();
 	}
 
 	std::string ShapeToDataName(int index, const std::string& shapeName) {
-		for (auto &tsn : targetshapenames)
-			if (tsn.second == shapeName)
-				return sliders[index].TargetDataName(tsn.first);
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end() && sliders.size() > index)
+			return sliders[index].TargetDataName(shape->second.targetShape);
 
 		return "";
 	}
 
 	std::string ShapeToTarget(const std::string& shapeName) {
-		for (auto &tsn : targetshapenames)
-			if (tsn.second == shapeName)
-				return tsn.first;
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end())
+			return shape->second.targetShape;
 
 		return "";
 	}
 
 	std::string ShapeToDataFolder(const std::string& shapeName) {
-		std::string t = ShapeToTarget(shapeName);
-		if (targetdatafolders.find(t) != targetdatafolders.end())
-			return targetdatafolders[t];
-		else
-			return datafolder;
+		auto shape = shapeAttributes.find(shapeName);
+		if (shape != shapeAttributes.end()) {
+			if (!shape->second.dataFolder.empty())
+				return shape->second.dataFolder;
+			else
+				return datafolder;
+		}
+
+		return "";
 	}
 
 	size_t size() {

src/components/SliderSet.cpp
@@ -84,12 +84,19 @@ int SliderSet::LoadSliderSet(XMLElement* element) {
 	XMLElement* shapeName = element->FirstChildElement(shapeStr.c_str());
 	while (shapeName) {
 		shapeName->SetName("Shape");
-		if (shapeName->Attribute("DataFolder"))
-			targetdatafolders[shapeName->Attribute("target")] = shapeName->Attribute("DataFolder");
-		else
-			targetdatafolders[shapeName->Attribute("target")] = datafolder;
 
-		targetshapenames[shapeName->Attribute("target")] = shapeName->GetText();
+		auto& shape = shapeAttributes[shapeName->GetText()];
+		if (shapeName->Attribute("DataFolder"))
+			shape.dataFolder = shapeName->Attribute("DataFolder");
+		else
+			shape.dataFolder = datafolder;
+
+		if (shapeName->Attribute("target"))
+			shape.targetShape = shapeName->Attribute("target");
+
+		if (shapeName->Attribute("SmoothSeamNormals"))
+			shape.smoothSeamNormals = shapeName->BoolAttribute("SmoothSeamNormals");
+
 		shapeName = shapeName->NextSiblingElement(shapeStr.c_str());
 	}
 
@@ -118,16 +125,15 @@ int SliderSet::LoadSliderSet(XMLElement* element) {
 	// A slider set/project can optionally specify a default normals generation configuration.  If present,
 	// it activates the normals generation functionality in the preview window. Note customized normals generation settings 
 	// are saved in separate xml files -- the project-homed ones are default settings only.
-	XMLElement* NormalsGeneration = element->FirstChildElement("NormalsGeneration");
-	if (NormalsGeneration != nullptr) {
-		// only load default settings if none are currently specified. 
-		if (defNormalGen.size() == 0) {
-			NormalGenLayer::LoadFromXML(NormalsGeneration, defNormalGen);
-		}
+	XMLElement* normalsGeneration = element->FirstChildElement("NormalsGeneration");
+	if (normalsGeneration) {
+		// Only load default settings if none are currently specified
+		if (defNormalGen.size() == 0)
+			NormalGenLayer::LoadFromXML(normalsGeneration, defNormalGen);
 	}
-	else {
+	else
 		defNormalGen.clear();
-	}
+
 	return 0;
 }
 
@@ -136,11 +142,12 @@ void SliderSet::LoadSetDiffData(DiffDataSets& inDataStorage) {
 
 	for (auto &slider : sliders) {
 		for (auto &ddf : slider.dataFiles) {
+			std::string shapeName = TargetToShape(ddf.targetName);
 			std::string fullFilePath = baseDataPath + "\\";
 			if (ddf.bLocal)
 				fullFilePath += datafolder + "\\";
 			else
-				fullFilePath += targetdatafolders[ddf.targetName] + "\\";
+				fullFilePath += ShapeToDataFolder(shapeName) + "\\";
 
 			fullFilePath += ddf.fileName;
 
@@ -196,14 +203,20 @@ void SliderSet::WriteSliderSet(XMLElement* sliderSetElement) {
 	XMLElement* sliderElement;
 	XMLElement* dataFileElement;
 
-	for (auto &tsn : targetshapenames) {
+	for (auto &s : shapeAttributes) {
 		newElement = sliderSetElement->GetDocument()->NewElement("Shape");
 		baseShapeElement = sliderSetElement->InsertEndChild(newElement)->ToElement();
-		baseShapeElement->SetAttribute("target", tsn.first.c_str());
-		if (targetdatafolders.find(tsn.first) != targetdatafolders.end())
-			baseShapeElement->SetAttribute("DataFolder", targetdatafolders[tsn.first].c_str());
 
-		newText = sliderSetElement->GetDocument()->NewText(tsn.second.c_str());
+		if (!s.second.targetShape.empty())
+			baseShapeElement->SetAttribute("target", s.second.targetShape.c_str());
+
+		if (!s.second.dataFolder.empty())
+			baseShapeElement->SetAttribute("DataFolder", s.second.dataFolder.c_str());
+
+		if (!s.second.smoothSeamNormals)
+			baseShapeElement->SetAttribute("SmoothSeamNormals", s.second.smoothSeamNormals);
+
+		newText = sliderSetElement->GetDocument()->NewText(s.first.c_str());
 		baseShapeElement->InsertEndChild(newText);
 	}
 

src/program/BodySlideApp.cpp
@@ -707,14 +707,14 @@ bool BodySlideApp::WriteMorphTRI(const std::string& triPath, SliderSet& sliderSe
 	TriFile tri;
 	std::string triFilePath = triPath + ".tri";
 
-	for (auto targetShape = sliderSet.TargetShapesBegin(); targetShape != sliderSet.TargetShapesEnd(); ++targetShape) {
-		NiShape* shape = nif.FindShapeByName(targetShape->second);
+	for (auto targetShape = sliderSet.ShapesBegin(); targetShape != sliderSet.ShapesEnd(); ++targetShape) {
+		NiShape* shape = nif.FindShapeByName(targetShape->first);
 		if (!shape)
 			continue;
 
 		for (int s = 0; s < sliderSet.size(); s++) {
-			std::string dn = sliderSet[s].TargetDataName(targetShape->first);
-			std::string target = targetShape->first;
+			std::string dn = sliderSet[s].TargetDataName(targetShape->second.targetShape);
+			std::string target = targetShape->second.targetShape;
 			if (dn.empty())
 				continue;
 
@@ -722,7 +722,7 @@ bool BodySlideApp::WriteMorphTRI(const std::string& triPath, SliderSet& sliderSe
 				MorphDataPtr morph = std::make_shared<MorphData>();
 				morph->name = sliderSet[s].name;
 
-				const std::vector<ushort>& shapeZapIndices = zapIndices[targetShape->second];
+				const std::vector<ushort>& shapeZapIndices = zapIndices[targetShape->first];
 
 				std::vector<Vector3> verts;
 				int shapeVertCount = shape->GetNumVertices();
@@ -748,7 +748,7 @@ bool BodySlideApp::WriteMorphTRI(const std::string& triPath, SliderSet& sliderSe
 				}
 
 				if (morph->offsets.size() > 0)
-					tri.AddMorph(targetShape->second, morph);
+					tri.AddMorph(targetShape->first, morph);
 			}
 		}
 	}
@@ -845,21 +845,21 @@ void BodySlideApp::InitPreview() {
 	std::vector<Vector3> verts;
 	std::vector<Vector2> uvs;
 	std::vector<ushort> zapIdx;
-	for (auto it = activeSet.TargetShapesBegin(); it != activeSet.TargetShapesEnd(); ++it) {
+	for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it) {
 		zapIdx.clear();
-		if (!previewBaseNif->GetVertsForShape(it->second, verts))
+		if (!previewBaseNif->GetVertsForShape(it->first, verts))
 			continue;
 
-		previewBaseNif->GetUvsForShape(it->second, uvs);
+		previewBaseNif->GetUvsForShape(it->first, uvs);
 
-		ApplySliders(it->first, sliderManager.slidersBig, verts, zapIdx, &uvs);
+		ApplySliders(it->second.targetShape, sliderManager.slidersBig, verts, zapIdx, &uvs);
 
 		// Zap deleted verts before preview
 		if (freshLoad && zapIdx.size() > 0) {
 			// Freshly loaded, need to actually delete verts and tris in the modified .nif
-			PreviewMod.SetVertsForShape(it->second, verts);
-			PreviewMod.SetUvsForShape(it->second, uvs);
-			PreviewMod.DeleteVertsForShape(it->second, zapIdx);
+			PreviewMod.SetVertsForShape(it->first, verts);
+			PreviewMod.SetUvsForShape(it->first, uvs);
+			PreviewMod.DeleteVertsForShape(it->first, zapIdx);
 		}
 		else if (zapIdx.size() > 0) {
 			// Preview Window has been opened for this shape before, zap the diff verts before applying them to the shape
@@ -869,19 +869,21 @@ void BodySlideApp::InitPreview() {
 				verts.erase(verts.begin() + zapIdx[z]);
 				uvs.erase(uvs.begin() + zapIdx[z]);
 			}
-			PreviewMod.SetVertsForShape(it->second, verts);
-			PreviewMod.SetUvsForShape(it->second, uvs);
+			PreviewMod.SetVertsForShape(it->first, verts);
+			PreviewMod.SetUvsForShape(it->first, uvs);
 		}
 		else {
 			// No zapping needed - just show all the verts.
-			PreviewMod.SetVertsForShape(it->second, verts);
-			PreviewMod.SetUvsForShape(it->second, uvs);
+			PreviewMod.SetVertsForShape(it->first, verts);
+			PreviewMod.SetUvsForShape(it->first, uvs);
 		}
 	}
 
 	std::string baseGamePath = Config["GameDataPath"];
 	preview->AddMeshFromNif(&PreviewMod);
 	preview->SetBaseDataPath(baseGamePath);
+
+	UpdateMeshesFromSet();
 
 	for (auto &s : PreviewMod.GetShapeNames())
 		preview->AddNifShapeTextures(&PreviewMod, s);
@@ -902,20 +904,20 @@ void BodySlideApp::UpdatePreview() {
 	std::vector<Vector3> verts, vertsLow, vertsHigh;
 	std::vector<Vector2> uvs, uvsLow, uvsHigh;
 	std::vector<ushort> zapIdx;
-	for (auto it = activeSet.TargetShapesBegin(); it != activeSet.TargetShapesEnd(); ++it) {
+	for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it) {
 		zapIdx.clear();
-		if (!previewBaseNif->GetVertsForShape(it->second, verts))
+		if (!previewBaseNif->GetVertsForShape(it->first, verts))
 			continue;
 
-		previewBaseNif->GetUvsForShape(it->second, uvs);
+		previewBaseNif->GetUvsForShape(it->first, uvs);
 		vertsHigh = verts;
 		vertsLow = verts;
 		uvsHigh = uvs;
 		uvsLow = uvs;
 
-		ApplySliders(it->first, sliderManager.slidersBig, vertsHigh, zapIdx, &uvsHigh);
+		ApplySliders(it->second.targetShape, sliderManager.slidersBig, vertsHigh, zapIdx, &uvsHigh);
 		if (activeSet.GenWeights())
-			ApplySliders(it->first, sliderManager.slidersSmall, vertsLow, zapIdx, &uvsLow);
+			ApplySliders(it->second.targetShape, sliderManager.slidersSmall, vertsLow, zapIdx, &uvsLow);
 
 		// Calculate result of weight
 		for (int i = 0; i < verts.size(); i++) {
@@ -933,7 +935,7 @@ void BodySlideApp::UpdatePreview() {
 				uvs.erase(uvs.begin() + zapIdx[z]);
 			}
 		}
-		preview->UpdateMeshes(it->second, &verts, &uvs);
+		preview->UpdateMeshes(it->first, &verts, &uvs);
 	}
 
 	preview->SetNormalsGenerationLayers(activeSet.GetNormalsGenLayers());
@@ -962,20 +964,20 @@ void BodySlideApp::RebuildPreviewMeshes() {
 	std::vector<Vector2> uvs, uvsLow, uvsHigh;
 	std::vector<ushort> zapIdx;
 	Vector3 v;
-	for (auto it = activeSet.TargetShapesBegin(); it != activeSet.TargetShapesEnd(); ++it) {
+	for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it) {
 		zapIdx.clear();
-		if (!previewBaseNif->GetVertsForShape(it->second, verts))
+		if (!previewBaseNif->GetVertsForShape(it->first, verts))
 			continue;
 
-		previewBaseNif->GetUvsForShape(it->second, uvs);
+		previewBaseNif->GetUvsForShape(it->first, uvs);
 		vertsHigh = verts;
 		vertsLow = verts;
 		uvsHigh = uvs;
 		uvsLow = uvs;
 
-		ApplySliders(it->first, sliderManager.slidersBig, vertsHigh, zapIdx, &uvsHigh);
+		ApplySliders(it->second.targetShape, sliderManager.slidersBig, vertsHigh, zapIdx, &uvsHigh);
 		if (activeSet.GenWeights())
-			ApplySliders(it->first, sliderManager.slidersSmall, vertsLow, zapIdx, &uvsLow);
+			ApplySliders(it->second.targetShape, sliderManager.slidersSmall, vertsLow, zapIdx, &uvsLow);
 		
 		// Calculate result of weight
 		for (int i = 0; i < verts.size(); i++) {
@@ -986,18 +988,31 @@ void BodySlideApp::RebuildPreviewMeshes() {
 		// Zap deleted verts before preview
 		if (zapIdx.size() > 0) {
 			// Freshly loaded, need to actually delete verts and tris in the modified .nif
-			PreviewMod.SetVertsForShape(it->second, verts);
-			PreviewMod.SetUvsForShape(it->second, uvs);
-			PreviewMod.DeleteVertsForShape(it->second, zapIdx);
+			PreviewMod.SetVertsForShape(it->first, verts);
+			PreviewMod.SetUvsForShape(it->first, uvs);
+			PreviewMod.DeleteVertsForShape(it->first, zapIdx);
 		}
 		else {
 			// No zapping needed - just show all the verts.
-			PreviewMod.SetVertsForShape(it->second, verts);
-			PreviewMod.SetUvsForShape(it->second, uvs);
+			PreviewMod.SetVertsForShape(it->first, verts);
+			PreviewMod.SetUvsForShape(it->first, uvs);
 		}
 	}
 
 	preview->RefreshMeshFromNif(&PreviewMod);
+	UpdateMeshesFromSet();
+}
+
+void BodySlideApp::UpdateMeshesFromSet() {
+	for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it) {
+		mesh* m = preview->GetMesh(it->first);
+		if (m) {
+			m->smoothSeamNormals = it->second.smoothSeamNormals;
+
+			if (!m->smoothSeamNormals)
+				m->SmoothNormals();
+		}
+	}
 }
 
 bool BodySlideApp::SetDefaultConfig() {
@@ -1573,39 +1588,39 @@ int BodySlideApp::BuildBodies(bool localPath, bool clean, bool tri) {
 	std::vector<ushort> zapIdx;
 	std::unordered_map<std::string, std::vector<ushort>> zapIdxAll;
 
-	for (auto it = activeSet.TargetShapesBegin(); it != activeSet.TargetShapesEnd(); ++it) {
-		if (!nifBig.GetVertsForShape(it->second, vertsHigh))
+	for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it) {
+		if (!nifBig.GetVertsForShape(it->first, vertsHigh))
 			continue;
 
-		nifBig.GetUvsForShape(it->second, uvsHigh);
+		nifBig.GetUvsForShape(it->first, uvsHigh);
 
 		if (activeSet.GenWeights()) {
-			if (!nifSmall.GetVertsForShape(it->second, vertsLow))
+			if (!nifSmall.GetVertsForShape(it->first, vertsLow))
 				continue;
 
-			nifSmall.GetUvsForShape(it->second, uvsLow);
+			nifSmall.GetUvsForShape(it->first, uvsLow);
 		}
 
-		zapIdxAll.emplace(it->second, std::vector<ushort>());
+		zapIdxAll.emplace(it->first, std::vector<ushort>());
 
-		ApplySliders(it->first, sliderManager.slidersBig, vertsHigh, zapIdx, &uvsHigh);
-		nifBig.SetVertsForShape(it->second, vertsHigh);
-		nifBig.SetUvsForShape(it->second, uvsHigh);
-		nifBig.CalcNormalsForShape(it->second);
-		nifBig.CalcTangentsForShape(it->second);
-		nifBig.DeleteVertsForShape(it->second, zapIdx);
+		ApplySliders(it->second.targetShape, sliderManager.slidersBig, vertsHigh, zapIdx, &uvsHigh);
+		nifBig.SetVertsForShape(it->first, vertsHigh);
+		nifBig.SetUvsForShape(it->first, uvsHigh);
+		nifBig.CalcNormalsForShape(it->first, it->second.smoothSeamNormals);
+		nifBig.CalcTangentsForShape(it->first);
+		nifBig.DeleteVertsForShape(it->first, zapIdx);
 
 		if (activeSet.GenWeights()) {
 			zapIdx.clear();
-			ApplySliders(it->first, sliderManager.slidersSmall, vertsLow, zapIdx, &uvsLow);
-			nifSmall.SetVertsForShape(it->second, vertsLow);
-			nifSmall.SetUvsForShape(it->second, uvsLow);
-			nifSmall.CalcNormalsForShape(it->second);
-			nifSmall.CalcTangentsForShape(it->second);
-			nifSmall.DeleteVertsForShape(it->second, zapIdx);
+			ApplySliders(it->second.targetShape, sliderManager.slidersSmall, vertsLow, zapIdx, &uvsLow);
+			nifSmall.SetVertsForShape(it->first, vertsLow);
+			nifSmall.SetUvsForShape(it->first, uvsLow);
+			nifSmall.CalcNormalsForShape(it->first, it->second.smoothSeamNormals);
+			nifSmall.CalcTangentsForShape(it->first);
+			nifSmall.DeleteVertsForShape(it->first, zapIdx);
 		}
 
-		zapIdxAll[it->second] = zapIdx;
+		zapIdxAll[it->first] = zapIdx;
 		zapIdx.clear();
 	}
 
@@ -1622,15 +1637,15 @@ int BodySlideApp::BuildBodies(bool localPath, bool clean, bool tri) {
 		}
 
 		if (targetGame < FO4) {
-			for (auto targetShape = activeSet.TargetShapesBegin(); targetShape != activeSet.TargetShapesEnd(); ++targetShape) {
-				NiShape* shape = nifBig.FindShapeByName(targetShape->second);
+			for (auto targetShape = activeSet.ShapesBegin(); targetShape != activeSet.ShapesEnd(); ++targetShape) {
+				NiShape* shape = nifBig.FindShapeByName(targetShape->first);
 				if (!shape)
 					continue;
 
 				if (tri && shape->GetNumVertices() > 0) {
-					AddTriData(nifBig, targetShape->second, triPathTrimmed);
+					AddTriData(nifBig, targetShape->first, triPathTrimmed);
 					if (activeSet.GenWeights())
-						AddTriData(nifSmall, targetShape->second, triPathTrimmed);
+						AddTriData(nifSmall, targetShape->first, triPathTrimmed);
 
 					tri = false;
 				}
@@ -1643,10 +1658,10 @@ int BodySlideApp::BuildBodies(bool localPath, bool clean, bool tri) {
 		}
 
 		// Set all shapes to dynamic/mutable
-		for (auto it = activeSet.TargetShapesBegin(); it != activeSet.TargetShapesEnd(); ++it) {
-			nifBig.SetShapeDynamic(it->second);
+		for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it) {
+			nifBig.SetShapeDynamic(it->first);
 			if (activeSet.GenWeights())
-				nifSmall.SetShapeDynamic(it->second);
+				nifSmall.SetShapeDynamic(it->first);
 		}
 	}
 	else {
@@ -1919,27 +1934,27 @@ int BodySlideApp::BuildListBodies(std::vector<std::string>& outfitList, std::map
 		std::vector<ushort> zapIdx;
 		std::unordered_map<std::string, std::vector<ushort>> zapIdxAll;
 
-		for (auto it = currentSet.TargetShapesBegin(); it != currentSet.TargetShapesEnd(); ++it) {
-			if (!nifBig.GetVertsForShape(it->second, vertsHigh))
+		for (auto it = currentSet.ShapesBegin(); it != currentSet.ShapesEnd(); ++it) {
+			if (!nifBig.GetVertsForShape(it->first, vertsHigh))
 				continue;
 
-			nifBig.GetUvsForShape(it->second, uvsHigh);
+			nifBig.GetUvsForShape(it->first, uvsHigh);
 
 			if (currentSet.GenWeights()) {
-				if (!nifSmall.GetVertsForShape(it->second, vertsLow))
+				if (!nifSmall.GetVertsForShape(it->first, vertsLow))
 					continue;
 
-				nifSmall.GetUvsForShape(it->second, uvsLow);
+				nifSmall.GetUvsForShape(it->first, uvsLow);
 			}
 
 			float vbig = 0.0f;
 			float vsmall = 0.0f;
 			std::vector<int> clamps;
-			zapIdxAll.emplace(it->second, std::vector<ushort>());
+			zapIdxAll.emplace(it->first, std::vector<ushort>());
 
 			for (int s = 0; s < currentSet.size(); s++) {
-				std::string dn = currentSet[s].TargetDataName(it->first);
-				std::string target = it->first;
+				std::string dn = currentSet[s].TargetDataName(it->second.targetShape);
+				std::string target = it->second.targetShape;
 				if (dn.empty())
 					continue;
 
@@ -1975,7 +1990,7 @@ int BodySlideApp::BuildListBodies(std::vector<std::string>& outfitList, std::map
 				if (currentSet[s].bZap && !currentSet[s].bUV) {
 					if (vbig > 0.0f) {
 						currentDiffs.GetDiffIndices(dn, target, zapIdx);
-						zapIdxAll[it->second] = zapIdx;
+						zapIdxAll[it->first] = zapIdx;
 					}
 					continue;
 				}
@@ -1995,8 +2010,8 @@ int BodySlideApp::BuildListBodies(std::vector<std::string>& outfitList, std::map
 
 			if (!clamps.empty()) {
 				for (auto &c : clamps) {
-					std::string dn = currentSet[c].TargetDataName(it->first);
-					std::string target = it->first;
+					std::string dn = currentSet[c].TargetDataName(it->second.targetShape);
+					std::string target = it->second.targetShape;
 					if (currentSet[c].defBigValue > 0)
 						currentDiffs.ApplyClamp(dn, target, &vertsHigh);
 
@@ -2006,18 +2021,18 @@ int BodySlideApp::BuildListBodies(std::vector<std::string>& outfitList, std::map
 				}
 			}
 
-			nifBig.SetVertsForShape(it->second, vertsHigh);
-			nifBig.SetUvsForShape(it->second, uvsHigh);
-			nifBig.CalcNormalsForShape(it->second);
-			nifBig.CalcTangentsForShape(it->second);
-			nifBig.DeleteVertsForShape(it->second, zapIdx);
+			nifBig.SetVertsForShape(it->first, vertsHigh);
+			nifBig.SetUvsForShape(it->first, uvsHigh);
+			nifBig.CalcNormalsForShape(it->first, it->second.smoothSeamNormals);
+			nifBig.CalcTangentsForShape(it->first);
+			nifBig.DeleteVertsForShape(it->first, zapIdx);
 
 			if (currentSet.GenWeights()) {
-				nifSmall.SetVertsForShape(it->second, vertsLow);
-				nifSmall.SetUvsForShape(it->second, uvsLow);
-				nifSmall.CalcNormalsForShape(it->second);
-				nifSmall.CalcTangentsForShape(it->second);
-				nifSmall.DeleteVertsForShape(it->second, zapIdx);
+				nifSmall.SetVertsForShape(it->first, vertsLow);
+				nifSmall.SetUvsForShape(it->first, uvsLow);
+				nifSmall.CalcNormalsForShape(it->first, it->second.smoothSeamNormals);
+				nifSmall.CalcTangentsForShape(it->first);
+				nifSmall.DeleteVertsForShape(it->first, zapIdx);
 			}
 
 			zapIdx.clear();
@@ -2051,15 +2066,15 @@ int BodySlideApp::BuildListBodies(std::vector<std::string>& outfitList, std::map
 			}
 
 			if (targetGame < FO4) {
-				for (auto targetShape = currentSet.TargetShapesBegin(); targetShape != currentSet.TargetShapesEnd(); ++targetShape) {
-					NiShape* shape = nifBig.FindShapeByName(targetShape->second);
+				for (auto targetShape = currentSet.ShapesBegin(); targetShape != currentSet.ShapesEnd(); ++targetShape) {
+					NiShape* shape = nifBig.FindShapeByName(targetShape->first);
 					if (!shape)
 						continue;
 
 					if (triEnd && shape->GetNumVertices() > 0) {
-						AddTriData(nifBig, targetShape->second, triPathTrimmed);
+						AddTriData(nifBig, targetShape->first, triPathTrimmed);
 						if (currentSet.GenWeights())
-							AddTriData(nifSmall, targetShape->second, triPathTrimmed);
+							AddTriData(nifSmall, targetShape->first, triPathTrimmed);
 
 						triEnd = false;
 					}
@@ -2072,10 +2087,10 @@ int BodySlideApp::BuildListBodies(std::vector<std::string>& outfitList, std::map
 			}
 
 			// Set all shapes to dynamic/mutable
-			for (auto it = currentSet.TargetShapesBegin(); it != currentSet.TargetShapesEnd(); ++it) {
-				nifBig.SetShapeDynamic(it->second);
+			for (auto it = currentSet.ShapesBegin(); it != currentSet.ShapesEnd(); ++it) {
+				nifBig.SetShapeDynamic(it->first);
 				if (currentSet.GenWeights())
-					nifSmall.SetShapeDynamic(it->second);
+					nifSmall.SetShapeDynamic(it->first);
 			}
 		}
 		else {

src/program/BodySlideApp.h
@@ -206,6 +206,7 @@ public:
 
 	void UpdatePreview();
 	void RebuildPreviewMeshes();
+	void UpdateMeshesFromSet();
 
 	int BuildBodies(bool localPath = false, bool clean = false, bool tri = false);
 	int BuildListBodies(std::vector<std::string>& outfitList, std::map<std::string, std::string>& failedOutfits, bool remove = false, bool tri = false, const std::string& custPath = "");

src/program/OutfitProject.cpp
@@ -88,6 +88,7 @@ std::string OutfitProject::Save(const wxString& strFileName,
 		// Add all the reference shapes to the target list.
 		outSet.AddShapeTarget(baseShape, ShapeToTarget(baseShape));
 		outSet.AddTargetDataFolder(ShapeToTarget(baseShape), activeSet.ShapeToDataFolder(baseShape));
+		outSet.SetSmoothSeamNormals(baseShape, activeSet.GetSmoothSeamNormals(baseShape));
 		owner->UpdateProgress(prog += step, _("Adding reference shapes..."));
 	}
 
@@ -103,6 +104,7 @@ std::string OutfitProject::Save(const wxString& strFileName,
 		if (shapeDataFolder != activeSet.GetDefaultDataFolder())
 			outSet.AddTargetDataFolder(ShapeToTarget(s), activeSet.ShapeToDataFolder(s));
 
+		outSet.SetSmoothSeamNormals(s, activeSet.GetSmoothSeamNormals(s));
 		owner->UpdateProgress(prog += step, _("Adding outfit shapes..."));
 	}
 	
@@ -975,9 +977,9 @@ void OutfitProject::GetLiveVerts(const std::string& shapeName, std::vector<Vecto
 }
 
 const std::string& OutfitProject::ShapeToTarget(const std::string& shapeName) {
-	for (auto it = activeSet.TargetShapesBegin(); it != activeSet.TargetShapesEnd(); ++it)
-		if (it->second == shapeName)
-			return it->first;
+	for (auto it = activeSet.ShapesBegin(); it != activeSet.ShapesEnd(); ++it)
+		if (it->first == shapeName)
+			return it->second.targetShape;
 
 	return shapeName;
 }
@@ -1784,12 +1786,12 @@ int OutfitProject::OutfitFromSliderSet(const std::string& fileName, const std::s
 
 	// No external target found, first skin shaded shape becomes reference
 	if (refTargets.empty()) {
-		for (auto shapeTarget = activeSet.TargetShapesBegin(); shapeTarget != activeSet.TargetShapesEnd(); ++shapeTarget) {
-			NiShape* shape = workNif.FindShapeByName(shapeTarget->second);
+		for (auto shapeTarget = activeSet.ShapesBegin(); shapeTarget != activeSet.ShapesEnd(); ++shapeTarget) {
+			NiShape* shape = workNif.FindShapeByName(shapeTarget->first);
 			if (shape) {
 				NiShader* shader = workNif.GetShader(shape);
 				if (shader && shader->IsSkinTinted()) {
-					newBaseShape = shapeTarget->second;
+					newBaseShape = shapeTarget->first;
 					break;
 				}
 			}

commit b5029ce88e06f7d647800e31f23d319c00b4cb3f

Fixed slider data regression from 35e56c3

src/components/SliderSet.h
@@ -235,7 +235,7 @@ public:
 				return datafolder;
 		}
 
-		return "";
+		return datafolder;
 	}
 
 	size_t size() {