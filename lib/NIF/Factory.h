7e290c219ee56f523c238832c5024d1954168bde
ousnius <ousnius@users.noreply.github.com>
2018-01-09 12:34:27 +0100

    New NifFile ctors, move NiFactory to own source

---
+++ lib/NIF/Factory.h
@@ -0,0 +1,69 @@
+/*
+BodySlide and Outfit Studio
+Copyright (C) 2018  Caliente & ousnius
+See the included LICENSE file
+*/
+
+#pragma once
+
+#include "BasicTypes.h"
+#include "Animation.h"
+#include "bhk.h"
+#include "ExtraData.h"
+#include "Geometry.h"
+#include "Keys.h"
+#include "Objects.h"
+#include "Particles.h"
+#include "Shaders.h"
+#include "Skin.h"
+
+#include <unordered_map>
+
+class NiFactory {
+public:
+	virtual NiObject* Create() = 0;
+	virtual NiObject* Load(NiStream& stream) = 0;
+};
+
+template<typename T>
+class NiFactoryType : public NiFactory {
+public:
+	// Create new NiObject
+	virtual NiObject* Create() override {
+		return new T();
+	}
+
+	// Load new NiObject from file
+	virtual NiObject* Load(NiStream& stream) override {
+		T* nio = new T();
+		nio->Get(stream);
+		return nio;
+	}
+};
+
+class NiFactoryRegister {
+public:
+	// Constructor registers the block types
+	NiFactoryRegister();
+
+	template<typename T>
+	void RegisterFactory() {
+		// Any NiObject can be registered together with its block name
+		m_registrations.emplace(T::BlockName, std::make_shared<NiFactoryType<T>>());
+	}
+
+	// Get block factory via header std::string
+	std::shared_ptr<NiFactory> GetFactoryByName(const std::string& name) {
+		auto it = m_registrations.find(name);
+		if (it != m_registrations.end())
+			return it->second;
+
+		return nullptr;
+	}
+
+	// Get static instance of factory register
+	static NiFactoryRegister& Get();
+
+protected:
+	std::unordered_map<std::string, std::shared_ptr<NiFactory>> m_registrations;
+};
--- lib/NIF/NifFile.h
+++ lib/NIF/NifFile.h
@@ -6,18 +6,7 @@ See the included LICENSE file
 
 #pragma once
 
-#include "BasicTypes.h"
-#include "Animation.h"
-#include "bhk.h"
-#include "ExtraData.h"
-#include "Geometry.h"
-#include "Keys.h"
-#include "Objects.h"
-#include "Particles.h"
-#include "Shaders.h"
-#include "Skin.h"
-
-#include <unordered_map>
+#include "Factory.h"
 
 struct OptOptionsSSE {
 	bool headParts = false;
@@ -33,56 +22,6 @@ struct OptResultSSE {
 	std::vector<std::string> shapesPartTriangulated;
 	std::vector<std::string> shapesTangentsAdded;
 	std::vector<std::string> shapesParallaxRemoved;
-};
-
-
-class IFactory {
-public:
-	virtual NiObject* Create() = 0;
-	virtual NiObject* Load(NiStream& stream) = 0;
-};
-
-template<typename T>
-class NiFactory : public IFactory {
-public:
-	// Create new NiObject
-	virtual NiObject* Create() override {
-		return new T();
-	}
-
-	// Load new NiObject from file
-	virtual NiObject* Load(NiStream& stream) override {
-		T* nio = new T();
-		nio->Get(stream);
-		return nio;
-	}
-};
-
-class NiFactoryRegister {
-public:
-	// Constructor registers the block types
-	NiFactoryRegister();
-
-	template<typename T>
-	void RegisterFactory() {
-		// Any NiObject can be registered together with its block name
-		m_registrations.emplace(T::BlockName, std::make_shared<NiFactory<T>>());
-	}
-
-	// Get block factory via header std::string
-	std::shared_ptr<IFactory> GetFactoryByName(const std::string& name) {
-		auto it = m_registrations.find(name);
-		if (it != m_registrations.end())
-			return it->second;
-
-		return nullptr;
-	}
-
-	// Get static instance of factory register
-	static NiFactoryRegister& GetNiFactoryRegister();
-
-protected:
-	std::unordered_map<std::string, std::shared_ptr<IFactory>> m_registrations;
 };
 
 // Sort bone weights with indices
@@ -107,6 +46,14 @@ private:
 
 public:
 	NifFile() {}
+
+	NifFile(const std::string& filename) {
+		Load(filename);
+	}
+
+	NifFile(std::fstream& file, const std::string& filename) {
+		Load(file, filename);
+	}
 
 	NifFile(const NifFile& other) {
 		CopyFrom(other);
