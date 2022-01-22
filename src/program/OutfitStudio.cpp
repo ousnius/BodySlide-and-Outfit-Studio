90fd10dd3837dfd0587c902e0aee0607ddfdc442
Daz <34233861+daz001@users.noreply.github.com>
2022-01-22 19:52:55 +0000

    Validate that an outfit is loaded

--- src/program/OutfitStudio.cpp
+++ src/program/OutfitStudio.cpp
@@ -3511,7 +3511,14 @@ void OutfitStudioFrame::OnConvertBodyReference(wxCommandEvent& WXUNUSED(event)) 
 		wxMessageBox(_("There are no valid shapes loaded!"), _("Error"));
 		return;
 	}
-	
+
+	auto shapes = project->GetWorkNif()->GetShapes();
+	auto baseShape = project->GetBaseShape();
+	if (baseShape && shapes.size() == 1) {
+		wxMessageBox(_("There is no outfit loaded!"), _("Error"));
+		return;
+	}
+
 	if (bEditSlider) {
 		wxMessageBox(_("You're currently editing slider data, please exit the slider's edit mode (pencil button) and try again."));
 		return;
