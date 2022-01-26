ousnius/BodySlide-and-Outfit-Studio

commit a3d8633eed3e074a987712760f30decb2eaf8126

Improve performance writing slider data

src/components/DiffData.cpp
@@ -97,11 +97,19 @@ bool OSDataFile::Write(const std::string& fileName) {
 		file.write(diffs.first.c_str(), nameLength);
 
 		diffSize = static_cast<uint16_t>(diffs.second.size());
-		file.write((char*)&diffSize, 2);
-		for (auto &diff : diffs.second) {
-			file.write((char*)&diff.first, 2);
-			file.write((char*)&diff.second, sizeof(Vector3));
+
+		std::vector<DiffStruct> diffData(diffSize);
+		diffData.resize(diffSize);
+
+		for (auto& diff : diffs.second) {
+			if (diff.first < diffSize) {
+				diffData[diff.first].index = diff.first;
+				diffData[diff.first].diff = diff.second;
+			}
 		}
+
+		file.write((char*)&diffSize, 2);
+		file.write((char*)diffData.data(), diffSize * sizeof(DiffStruct));
 	}
 
 	return true;
@@ -223,11 +231,11 @@ bool DiffDataSets::SaveData(const std::map<std::string, std::map<std::string, st
 	for (auto &osd : osdNames) {
 		OSDataFile osdFile;
 		for (auto &dataNames : osd.second) {
-			std::unordered_map<uint16_t, Vector3>* data = &namedSet[dataNames.first];
+			auto& data = namedSet[dataNames.first];
 			if (!TargetMatch(dataNames.first, dataNames.second))
 				continue;
 
-			osdFile.SetDataDiff(dataNames.first, *data);
+			osdFile.SetDataDiff(dataNames.first, data);
 		}
 
 		if (!osdFile.Write(osd.first))

src/components/Automorph.cpp
@@ -254,11 +254,7 @@ void Automorph::GetRawResultDiff(const std::string& shapeName, const std::string
 	if (!resultDiffData.TargetMatch(setName, shapeName))
 		return;
 
-	outDiff.clear();
-
-	std::unordered_map<uint16_t, Vector3>* set = resultDiffData.GetDiffSet(setName);
-	for (auto &i : *set)
-		outDiff[i.first] = i.second;
+	outDiff = *resultDiffData.GetDiffSet(setName);
 }
 
 int Automorph::GetResultDiffSize(const std::string& shapeName, const std::string& sliderName) {

commit fb27bfc1d23de8c2774718a484471fa1686fc87f

Fix regression caused by a3d8633 (writing OSD)

src/components/DiffData.cpp
@@ -100,11 +100,15 @@ bool OSDataFile::Write(const std::string& fileName) {
 		std::vector<DiffStruct> diffData(diffSize);
 		diffData.resize(diffSize);
 
+		size_t i = 0;
+
 		for (auto& diff : diffs.second) {
-			if (diff.first < diffSize) {
-				diffData[diff.first].index = diff.first;
-				diffData[diff.first].diff = diff.second;
-			}
+			if (i >= diffSize)
+				break;
+
+			diffData[i].index = diff.first;
+			diffData[i].diff = diff.second;
+			++i;
 		}
 
 		file.write((char*)&diffSize, 2);