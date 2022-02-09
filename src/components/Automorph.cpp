ousnius/BodySlide-and-Outfit-Studio

commit fdf6f939607627c7494707ea59ce7c7a3c154531

Fixed automorph not taking mesh-to-model transformation into account

src/components/Automorph.cpp
@@ -451,7 +451,7 @@ void Automorph::GenerateResultDiff(
 			continue;
 
 		if (!solidMode) {
-			(*resultDiffSet)[i] = totalMove;
+			(*resultDiffSet)[i] = m->DiffModelToMesh(morphRef->DiffMeshToModel(totalMove));
 		}
 		else {
 			totalMoveList.reserve(m->nVerts);
@@ -503,7 +503,7 @@ void Automorph::GenerateResultDiff(
 			if (totalMove.IsZero(true))
 				continue;
 
-			(*resultDiffSet)[i] = totalMove;
+			(*resultDiffSet)[i] = m->DiffModelToMesh(morphRef->DiffMeshToModel(totalMove));
 		}
 	}
 }

commit ce306987c9aade88c16d1deabaeda4cc020dbc49

Fixed automorph bug for weights if transform has rot or scale. The bug was introduced in commit fdf6f93.

src/components/Automorph.cpp
@@ -363,7 +363,7 @@ std::string Automorph::ResultDataName(const std::string& shapeName, const std::s
 }
 
 void Automorph::GenerateResultDiff(
-	const std::string& shapeName, const std::string& sliderName, const std::string& refDataName, int maxResults, bool noSqueeze, bool solidMode, bool axisX, bool axisY, bool axisZ) {
+	const std::string& shapeName, const std::string& sliderName, const std::string& refDataName, bool transformResults, int maxResults, bool noSqueeze, bool solidMode, bool axisX, bool axisY, bool axisZ) {
 	if (sourceShapes.find(shapeName) == sourceShapes.end())
 		return;
 
@@ -373,6 +373,12 @@ void Automorph::GenerateResultDiff(
 
 	mesh* m = sourceShapes[shapeName];
 	std::string dataName = shapeName + sliderName;
+
+	MatTransform transform;
+	if (transformResults) {
+		transform = m->xformModelToMesh.ComposeTransforms(morphRef->xformMeshToModel);
+		transformResults = !transform.IsNearlyEqualTo(MatTransform());
+	}
 
 	if (resultDiffData.TargetMatch(dataName, shapeName)) {
 		if (m->mask)
@@ -451,7 +457,9 @@ void Automorph::GenerateResultDiff(
 			continue;
 
 		if (!solidMode) {
-			(*resultDiffSet)[i] = m->TransformDiffModelToMesh(morphRef->TransformDiffMeshToModel(totalMove));
+			if (transformResults)
+				totalMove = transform.ApplyTransformToDiff(totalMove);
+			(*resultDiffSet)[i] = totalMove;
 		}
 		else {
 			totalMoveList.reserve(m->nVerts);
@@ -503,7 +511,9 @@ void Automorph::GenerateResultDiff(
 			if (totalMove.IsZero(true))
 				continue;
 
-			(*resultDiffSet)[i] = m->TransformDiffModelToMesh(morphRef->TransformDiffMeshToModel(totalMove));
+			if (transformResults)
+				totalMove = transform.ApplyTransformToDiff(totalMove);
+			(*resultDiffSet)[i] = totalMove;
 		}
 	}
 }

src/components/Automorph.h
@@ -70,6 +70,7 @@ public:
 	void GenerateResultDiff(const std::string& shapeName,
 							const std::string& sliderName,
 							const std::string& refDataName,
+							bool transformResults,
 							int maxResults = 10,
 							bool noSqueeze = false,
 							bool solidMode = false,

src/program/OutfitProject.cpp
@@ -1589,7 +1589,7 @@ void OutfitProject::CopyBoneWeights(NiShape* shape,
 
 		// Calculate new values for bone's weights
 		std::string wtSet = boneName + "_WT_";
-		morpher.GenerateResultDiff(shapeName, wtSet, wtSet, maxResults);
+		morpher.GenerateResultDiff(shapeName, wtSet, wtSet, false, maxResults);
 
 		std::unordered_map<uint16_t, Vector3> diffResult;
 		morpher.GetRawResultDiff(shapeName, wtSet, diffResult);
@@ -2269,6 +2269,7 @@ void OutfitProject::ConformShape(NiShape* shape, const ConformOptions& options) 
 			morpher.GenerateResultDiff(shape->name.get(),
 									   activeSet[i].name,
 									   activeSet[i].TargetDataName(refTarget),
+									   true,
 									   options.maxResults,
 									   options.noSqueeze,
 									   options.solidMode,