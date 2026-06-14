ousnius/BodySlide-and-Outfit-Studio

commit a07987b6abf6810fdc42310e0f274509172318d5

BodySlide: Use game's data path if there's no project files in app dir (#588)
* BodySlide: Use game's data path if there's no project files in appdir
* Add ProjectUtil and centralize project path

Introduce ProjectUtil::GetProjectPath() (src/utils/ProjectUtil.{h,cpp}) to centralize project path resolution and prefer configured ProjectPath with sensible fallbacks.

Replace numerous ad-hoc GetProjectPath() uses across AutomationDialog, BodySlideApp, OutfitStudio, GroupManager, OutfitProject, ConvertBodyReferenceDialog, PresetSaveDialog, and related headers/impls to call ProjectUtil::GetProjectPath(). Remove several local GetProjectPath wrappers and update build files (BodySlide.vcxproj, OutfitStudio.vcxproj, CMakeLists.txt) to include the new source.

The new resolver checks Config["ProjectPath"] first and then falls back to AppDir/SliderSets, GameDataPath/CalienteTools/BodySlide, and GameDataPath/Tools/BodySlide, returning the first existing path or AppDir.

---------

Co-authored-by: ousnius <ousnius@users.noreply.github.com>

src/utils/ProjectUtil.cpp
@@ -1,0 +1,41 @@
+/*
+BodySlide and Outfit Studio
+See the included LICENSE file
+*/
+
+#include "ProjectUtil.h"
+#include "ConfigurationManager.h"
+#include "StringStuff.h"
+
+#include <wx/dir.h>
+
+extern ConfigurationManager Config;
+
+std::string ProjectUtil::GetProjectPath() {
+	std::string projectPath = Config["ProjectPath"];
+	std::string appDir = Config["AppDir"];
+	std::string gameDataPath = Config["GameDataPath"];
+
+	// Build list of paths to check in order of preference
+	std::vector<std::string> pathsToCheck;
+
+	// First priority: configured ProjectPath (if set)
+	if (!projectPath.empty()) {
+		pathsToCheck.push_back(projectPath);
+	}
+
+	// Fallback paths in order of preference
+	pathsToCheck.push_back(appDir + PathSepStr + "SliderSets");
+	pathsToCheck.push_back(gameDataPath + PathSepStr + "CalienteTools" + PathSepStr + "BodySlide");
+	pathsToCheck.push_back(gameDataPath + PathSepStr + "Tools" + PathSepStr + "BodySlide");
+
+	// Return first existing path
+	for (const auto& path : pathsToCheck) {
+		if (wxDir::Exists(path)) {
+			return path;
+		}
+	}
+
+	// If no path exists, return projectPath if configured, otherwise AppDir
+	return !projectPath.empty() ? projectPath : appDir;
+}

BodySlide.vcxproj
@@ -794,6 +794,7 @@
     <ClInclude Include="src\utils\Log.h" />
     <ClInclude Include="src\utils\PlatformUtil.h" />
     <ClInclude Include="src\utils\SettingsDialogShared.h" />
+    <ClInclude Include="src\utils\ProjectUtil.h" />
     <ClInclude Include="src\utils\StackTrace.h" />
     <ClInclude Include="src\utils\StringStuff.h" />
   </ItemGroup>
@@ -869,6 +870,7 @@
     <ClCompile Include="src\utils\Log.cpp" />
     <ClCompile Include="src\utils\PlatformUtil.cpp" />
     <ClCompile Include="src\utils\SettingsDialogShared.cpp" />
+    <ClCompile Include="src\utils\ProjectUtil.cpp" />
     <ClCompile Include="src\utils\StackTrace.cpp" />
     <ClCompile Include="src\utils\StringStuff.cpp" />
     <ClCompile Include="src\utils\WxBuildConfigCheck.cpp" />

CMakeLists.txt
@@ -272,6 +272,7 @@ set(BSOS_COMMON_SOURCES
 	src/utils/Log.cpp
 	src/utils/PlatformUtil.cpp
 	src/utils/SettingsDialogShared.cpp
+	src/utils/ProjectUtil.cpp
 	src/utils/StackTrace.cpp
 	src/utils/StringStuff.cpp
 	src/utils/WxBuildConfigCheck.cpp

OutfitStudio.vcxproj
@@ -866,6 +866,7 @@
     <ClInclude Include="src\utils\Log.h" />
     <ClInclude Include="src\utils\PlatformUtil.h" />
     <ClInclude Include="src\utils\SettingsDialogShared.h" />
+    <ClInclude Include="src\utils\ProjectUtil.h" />
     <ClInclude Include="src\utils\StackTrace.h" />
     <ClInclude Include="src\utils\StringStuff.h" />
   </ItemGroup>
@@ -962,6 +963,7 @@
     <ClCompile Include="src\utils\Log.cpp" />
     <ClCompile Include="src\utils\PlatformUtil.cpp" />
     <ClCompile Include="src\utils\SettingsDialogShared.cpp" />
+    <ClCompile Include="src\utils\ProjectUtil.cpp" />
     <ClCompile Include="src\utils\StackTrace.cpp" />
     <ClCompile Include="src\utils\StringStuff.cpp" />
     <ClCompile Include="src\utils\WxBuildConfigCheck.cpp" />

src/program/AutomationDialog.cpp
@@ -10,6 +10,7 @@ See the included LICENSE file
 #include "PartitionTypeChoices.h"
 
 #include "../files/MaskFile.h"
+#include "../utils/ProjectUtil.h"
 
 #include <NifFile.hpp>
 
@@ -1200,7 +1201,7 @@ void AutomationDialog::UpdateUIFromStep(const AutomationStep& step) {
 				if (!path.IsEmpty()) {
 					wxFileName fn(path);
 					if (fn.IsRelative()) {
-						std::string projPath = GetProjectPath();
+						std::string projPath = ProjectUtil::GetProjectPath();
 						fn.MakeAbsolute(wxString::FromUTF8(projPath));
 					}
 					fp->SetPath(fn.GetFullPath());
@@ -1260,7 +1261,7 @@ void AutomationDialog::UpdateUIFromStep(const AutomationStep& step) {
 				if (!path.IsEmpty()) {
 					wxFileName fn(path);
 					if (fn.IsRelative()) {
-						std::string projPath = GetProjectPath();
+						std::string projPath = ProjectUtil::GetProjectPath();
 						fn.MakeAbsolute(wxString::FromUTF8(projPath));
 					}
 					fp->SetPath(fn.GetFullPath());
@@ -1575,7 +1576,7 @@ void AutomationDialog::UpdateUIFromStep(const AutomationStep& step) {
 				if (!path.IsEmpty()) {
 					wxFileName fn(path);
 					if (fn.IsRelative()) {
-						std::string projPath = GetProjectPath();
+						std::string projPath = ProjectUtil::GetProjectPath();
 						fn.MakeAbsolute(wxString::FromUTF8(projPath));
 					}
 					fp->SetPath(fn.GetFullPath());
@@ -2049,7 +2050,7 @@ std::vector<NiShape*> AutomationDialog::ResolveTargetShapes(const AutomationStep
 }
 
 std::string AutomationDialog::GetAutomationsFolder() {
-	return GetProjectPath() + "/Automations";
+	return ProjectUtil::GetProjectPath() + "/Automations";
 }
 
 void AutomationDialog::CollectScripts(const wxString& baseFolder, const wxString& currentFolder, std::vector<std::pair<wxString, wxString>>& entries) {
@@ -2343,7 +2344,7 @@ int AutomationDialog::RunHeadless(const wxString& scriptName, const wxArrayStrin
 		}
 		else {
 			// Resolve set project names against <ProjectPath>/SliderSets/*.{osp,xml}.
-			std::string projPath = GetProjectPath();
+			std::string projPath = ProjectUtil::GetProjectPath();
 			wxArrayString files;
 			wxDir::GetAllFiles(wxString::FromUTF8(projPath) + "/SliderSets", &files, "*.osp");
 			wxDir::GetAllFiles(wxString::FromUTF8(projPath) + "/SliderSets", &files, "*.xml");
@@ -3006,7 +3007,7 @@ void AutomationDialog::OnRefTemplateChanged(wxCommandEvent& WXUNUSED(event)) {
 		if (!source.IsEmpty()) {
 			wxFileName fn(source);
 			if (fn.IsRelative()) {
-				std::string projPath = GetProjectPath();
+				std::string projPath = ProjectUtil::GetProjectPath();
 				fn.MakeAbsolute(wxString::FromUTF8(projPath));
 			}
 			fp->SetPath(fn.GetFullPath());
@@ -3375,7 +3376,7 @@ wxString AutomationDialog::MakeRelativeToProject(const wxString& absolutePath) c
 	if (fn.IsRelative())
 		return absolutePath; // Already relative
 
-	if (fn.MakeRelativeTo(wxString::FromUTF8(GetProjectPath())))
+	if (fn.MakeRelativeTo(wxString::FromUTF8(ProjectUtil::GetProjectPath())))
 		return fn.GetFullPath();
 
 	return absolutePath; // Couldn't make relative, return as-is
@@ -3389,7 +3390,7 @@ wxString AutomationDialog::MakeAbsoluteToProject(const wxString& path) const {
 	if (!fn.IsRelative())
 		return path;
 
-	fn.MakeAbsolute(wxString::FromUTF8(GetProjectPath()));
+	fn.MakeAbsolute(wxString::FromUTF8(ProjectUtil::GetProjectPath()));
 	return fn.GetFullPath();
 }
 
@@ -3727,7 +3728,7 @@ std::vector<std::string> AutomationDialog::GatherBatchFiles() {
 std::vector<std::pair<std::string, std::string>> AutomationDialog::GatherBatchSliderSets() {
 	std::vector<std::pair<std::string, std::string>> result; // (fileName, setName) pairs
 
-	std::string projPath = GetProjectPath();
+	std::string projPath = ProjectUtil::GetProjectPath();
 	wxArrayString files;
 	wxDir::GetAllFiles(wxString::FromUTF8(projPath) + "/SliderSets", &files, "*.osp");
 	wxDir::GetAllFiles(wxString::FromUTF8(projPath) + "/SliderSets", &files, "*.xml");

src/program/BodySlideApp.cpp
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../utils/ParallelFor.h"
 #include "../utils/StackTrace.h"
 #include "../utils/StringStuff.h"
+#include "../utils/ProjectUtil.h"
 
 #include <algorithm>
 #include <atomic>
@@ -598,7 +599,7 @@ int BodySlideApp::CreateSetSliders(const std::string& outfit) {
 		activeSet.Clear();
 		sliderManager.ClearSliders();
 		if (!sliderDoc.GetSet(outfit, activeSet)) {
-			activeSet.SetBaseDataPath(GetProjectPath() + PathSepStr + "ShapeData");
+			activeSet.SetBaseDataPath(ProjectUtil::GetProjectPath() + PathSepStr + "ShapeData");
 			sliderManager.AddSlidersInSet(activeSet);
 			DisplayActiveSet();
 		}
@@ -631,7 +632,7 @@ int BodySlideApp::AddProjectSliders(const std::string& projectFile, const std::s
 	if (sliderDoc.GetSet(setName, pp->sliderSet))
 		return 3;
 
-	pp->sliderSet.SetBaseDataPath(GetProjectPath() + PathSepStr + "ShapeData");
+	pp->sliderSet.SetBaseDataPath(ProjectUtil::GetProjectPath() + PathSepStr + "ShapeData");
 	pp->setName = setName;
 
 	// Add sliders from this set (additive)
@@ -646,10 +647,6 @@ std::string BodySlideApp::GetOutputDataPath() const {
 	return res.empty() ? Config["GameDataPath"] : res;
 }
 
-std::string BodySlideApp::GetProjectPath() const {
-	std::string res = Config["ProjectPath"];
-	return res.empty() ? Config["AppDir"] : res;
-}
 
 bool BodySlideApp::PresetExists(const std::string& name) {
 	if (name.empty())
@@ -809,8 +806,8 @@ int BodySlideApp::LoadSliderSets() {
 	outFileCount.clear();
 
 	wxArrayString files;
-	wxDir::GetAllFiles(wxString::FromUTF8(GetProjectPath()) + "/SliderSets", &files, "*.osp");
-	wxDir::GetAllFiles(wxString::FromUTF8(GetProjectPath()) + "/SliderSets", &files, "*.xml");
+	wxDir::GetAllFiles(wxString::FromUTF8(ProjectUtil::GetProjectPath()) + "/SliderSets", &files, "*.osp");
+	wxDir::GetAllFiles(wxString::FromUTF8(ProjectUtil::GetProjectPath()) + "/SliderSets", &files, "*.xml");
 
 	bool filterHasZaps = false;
 
@@ -2274,7 +2271,7 @@ bool BodySlideApp::LoadExternalReference(const SliderSet& sliderSet) {
 	// Resolve project file path relative to project directory
 	wxFileName refProjectFileName(wxString::FromUTF8(projectFile));
 	if (refProjectFileName.IsRelative())
-		refProjectFileName.MakeAbsolute(wxString::FromUTF8(GetProjectPath()));
+		refProjectFileName.MakeAbsolute(wxString::FromUTF8(ProjectUtil::GetProjectPath()));
 
 	std::string projectFilePath = refProjectFileName.GetFullPath().ToUTF8().data();
 
@@ -2291,7 +2288,7 @@ bool BodySlideApp::LoadExternalReference(const SliderSet& sliderSet) {
 		return false;
 	}
 
-	referenceSliderSet.SetBaseDataPath(GetProjectPath() + PathSepStr + "ShapeData");
+	referenceSliderSet.SetBaseDataPath(ProjectUtil::GetProjectPath() + PathSepStr + "ShapeData");
 
 	// Load diff data for the reference shape
 	referenceDiffData.Clear();
@@ -3091,7 +3088,7 @@ void BodySlideApp::InitLanguage() {
 void BodySlideApp::LoadAllCategories() {
 	wxLogMessage("Loading all slider categories...");
 	cCollection.Clear();
-	cCollection.LoadCategories(GetProjectPath() + "/SliderCategories");
+	cCollection.LoadCategories(ProjectUtil::GetProjectPath() + "/SliderCategories");
 }
 
 void BodySlideApp::SetPresetGroups(const std::string& setName) {
@@ -3121,7 +3118,7 @@ void BodySlideApp::SetPresetGroups(const std::string& setName) {
 
 void BodySlideApp::LoadAllGroups() {
 	wxLogMessage("Loading all slider groups...");
-	gCollection.LoadGroups(GetProjectPath() + "/SliderGroups");
+	gCollection.LoadGroups(ProjectUtil::GetProjectPath() + "/SliderGroups");
 
 	ungroupedOutfits.clear();
 	for (auto& o : outfitNameSource) {
@@ -3355,7 +3352,7 @@ void BodySlideApp::LoadPresets(const std::string& sliderSet) {
 				groups_and_aliases.push_back(ag.first);
 	}
 
-	sliderManager.LoadPresets(GetProjectPath() + "/SliderPresets", outfit, groups_and_aliases, groups_and_aliases.empty());
+	sliderManager.LoadPresets(ProjectUtil::GetProjectPath() + "/SliderPresets", outfit, groups_and_aliases, groups_and_aliases.empty());
 }
 
 void BodySlideApp::GetPresetNames(std::vector<std::string>& outNames) {
@@ -4230,7 +4227,7 @@ int BodySlideApp::BuildListBodies(
 			return;
 		}
 
-		currentSet.SetBaseDataPath(GetProjectPath() + PathSepStr + "ShapeData");
+		currentSet.SetBaseDataPath(ProjectUtil::GetProjectPath() + PathSepStr + "ShapeData");
 
 		// ALT key
 		if (clean && custPath.empty()) {
@@ -4717,7 +4714,7 @@ void BodySlideApp::GroupBuild(const std::vector<std::string>& groupNames) {
 	}
 
 	std::vector<std::string> groups;
-	sliderManager.LoadPresets(GetProjectPath() + "/SliderPresets", "", groups, true);
+	sliderManager.LoadPresets(ProjectUtil::GetProjectPath() + "/SliderPresets", "", groups, true);
 
 	// Apply saved build selections for CLI group builds before entering batch build conflict handling.
 	BuildSelectionFile buildSelFile;
@@ -5838,7 +5835,7 @@ void BodySlideFrame::OnBrowseOutfitFolder(wxCommandEvent& WXUNUSED(event)) {
 
 	wxFileName folderPath(wxString::FromUTF8(activeSet.GetInputFileName()));
 	if (folderPath.IsRelative())
-		folderPath.MakeAbsolute(wxString::FromUTF8(app->GetProjectPath()));
+		folderPath.MakeAbsolute(wxString::FromUTF8(ProjectUtil::GetProjectPath()));
 
 	folderPath.SetFullName("");
 
@@ -5852,7 +5849,7 @@ void BodySlideFrame::OnSaveGroups(wxCommandEvent& WXUNUSED(event)) {
 
 	wxFileDialog saveGroupDialog(this,
 								 _("Choose or create group file"),
-								 wxString::FromUTF8(app->GetProjectPath()) + "/SliderGroups",
+								 wxString::FromUTF8(ProjectUtil::GetProjectPath()) + "/SliderGroups",
 								 wxEmptyString,
 								 "Group Files (*.xml)|*.xml",
 								 wxFD_SAVE);

src/program/BodySlideApp.h
@@ -174,7 +174,6 @@ public:
 	wxString GetGameDataPath(TargetGame targ);
 
 	std::string GetOutputDataPath() const;
-	std::string GetProjectPath() const;
 	SliderSet& GetActiveSet() { return projects[0]->sliderSet; }
 	DiffDataSets& GetActiveDataSets() { return projects[0]->dataSets; }
 	bool HasActiveProject() const { return !projects.empty(); }

src/program/ConvertBodyReferenceDialog.cpp
@@ -9,6 +9,7 @@ See the included LICENSE file
 
 #include "../utils/ConfigDialogUtil.h"
 #include "../utils/ConfigurationManager.h"
+#include "../utils/ProjectUtil.h"
 
 #include <regex>
 
@@ -329,7 +330,7 @@ int ConvertBodyReferenceDialog::LoadReferenceTemplate(const wxString& refTemplat
 	auto tmpl = find_if(refTemplates.begin(), refTemplates.end(), [&tmplName](const RefTemplate& rt) { return rt.GetName() == tmplName; });
 	if (tmpl != refTemplates.end()) {
 		if (wxFileName(wxString::FromUTF8(tmpl->GetSource())).IsRelative())
-			error = project->LoadReferenceTemplate(GetProjectPath() + PathSepStr + tmpl->GetSource(),
+			error = project->LoadReferenceTemplate(ProjectUtil::GetProjectPath() + PathSepStr + tmpl->GetSource(),
 												   tmpl->GetSetName(),
 												   tmpl->GetShape(),
 												   tmpl->GetLoadAll(),

commit ed7c28aaccd330d3744f4f39cf8d5743d93773c8

Fix project dir from app dir
Regression from a07987b6abf6810fdc42310e0f274509172318d5

src/utils/ProjectUtil.cpp
@@ -24,8 +24,12 @@ std::string ProjectUtil::GetProjectPath() {
 		pathsToCheck.push_back(projectPath);
 	}
 
+	// Check if SliderSets subdirectory exists in appDir, but return appDir if it does
+	if (wxDir::Exists(appDir + PathSepStr + "SliderSets")) {
+		return appDir;
+	}
+
 	// Fallback paths in order of preference
-	pathsToCheck.push_back(appDir + PathSepStr + "SliderSets");
 	pathsToCheck.push_back(gameDataPath + PathSepStr + "CalienteTools" + PathSepStr + "BodySlide");
 	pathsToCheck.push_back(gameDataPath + PathSepStr + "Tools" + PathSepStr + "BodySlide");
 